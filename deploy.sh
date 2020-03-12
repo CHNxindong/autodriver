75fcd27ccc230c40ed5238dee523c97295c4430f
CHNxindong <2639344149@qq.com>
2020-03-12 22:25:52 +0800

    deploy.sh修改3。

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
-cd %WORKSPACE%\auto_driver
-mvn clean
-mvn install
-mvn package
-cd %WORKSPACE%\auto_driver\target
-java -jar OnlineJudge-1.0-SNAPSHOT.jar
+#jenkins中命令合成一行，可以增加step（每个step仅一行，保证可以每个step按序执行）
+mvn clean install package
+#linux中可以用nohup java -jar ...jar实现后台运行
+java -jar %WORKSPACE%\target\OnlineJudge-1.0-SNAPSHOT.jar
